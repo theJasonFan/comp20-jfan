dbdff5ba9a90a122ceed51d65a36c89b542c943d
Jason Fan <jason.fan.74@gmail.com>
2015-02-28 21:18:03 -0500

    fine tuned image sizes and locations

--- mspacman/game.js
+++ mspacman/game.js
@@ -3,9 +3,8 @@ function init() {
     var ctx = canvas.getContext('2d');
     var img = new Image();
     img.src = 'pacman10-hp-sprite.png';
-
     img.onload = function() {
-        ctx.drawImage(img, 322, 2, 465, 136, 0, 0, 465, 136);
-        ctx.drawImage(img, 82, 20, 20, 20, 35, 25, 20, 20)
+        ctx.drawImage(img, 322, 2, 465, 136, 0, 0, 465, 136); // background img
+        ctx.drawImage(img, 82, 20, 20, 20, 35, 25, 20, 20); // ms pacman
     }
 }
