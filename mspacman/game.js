c28511f320d117a0898ac36658ba47b2a58f3ef9
Jason Fan <Jason_Fan@wr-130-64-223-150.medford.tufts.edu>
2015-02-28 20:24:30 -0500

    first draft, sliced and placed mspacman

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,11 @@
-// Replace this with your code...
+function init() {
+    var canvas = document.getElementById('game_canvas');
+    var ctx = canvas.getContext('2d');
+    var img = new Image();
+    img.src = 'pacman10-hp-sprite.png';
+
+    img.onload = function() {
+        ctx.drawImage(img, 322, 2, 465, 136, 0, 0, 465, 136);
+        ctx.drawImage(img, 82, 20, 20, 20, 40, 25, 20, 20)
+    }
+}
