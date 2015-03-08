bde72a5f15e27419dbcc3efec97b606b35845923
Jason Fan <jason.fan.74@gmail.com>
2015-03-08 15:42:48 -0400

    testing javascript pre-json parse

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,16 @@
 // Your JavaScript goes here...
+function parse()
+{
+        request = new XMLHttpRequest();
+        request.open("GET", "data.json",true);
+        request.onreadystatechange = parseData; // prepare function to be called
+        request.send(); //Exececute request
+}
+
+parseData() {
+        if (request.readyState == 4 && request.status == 200) {
+            toUpdate = document.getElementByID("messages");
+            toUpdate.innerHTML += '<p class="msg">' + request.data + '</p>';
+        }
+    
+}
