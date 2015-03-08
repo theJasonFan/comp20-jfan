a6b2b6472a57e31d073909172edfc12a5c3de56a
Jason Fan <jason.fan.74@gmail.com>
2015-03-08 17:48:16 -0400

    cleanup => lab.js indentation

--- messages/lab.js
+++ messages/lab.js
@@ -6,19 +6,19 @@
 
 function parse()
 {
-        request = new XMLHttpRequest();
-        request.open("GET", "data.json",true);
-        request.onreadystatechange = parseData;
-        request.send();
+    request = new XMLHttpRequest();
+    request.open("GET", "data.json",true);
+    request.onreadystatechange = parseData;
+    request.send();
 }
 
 function parseData() 
 {
-        if (request.readyState == 4 && request.status == 200) {
-            toUpdate = document.getElementById("messages");
-            listObjects = JSON.parse(request.responseText);
-            for ( i = 0; i < listObjects.length; i++)
-                toUpdate.innerHTML += '<p><span class="msg">' + listObjects[i].content + 
-                                      ' <span class="username">' + listObjects[i].username + '</span></span></p>';
-        }
+    if (request.readyState == 4 && request.status == 200) {
+        toUpdate = document.getElementById("messages");
+        listObjects = JSON.parse(request.responseText);
+        for ( i = 0; i < listObjects.length; i++)
+            toUpdate.innerHTML += '<p><span class="msg">' + listObjects[i].content + 
+                                  ' <span class="username">' + listObjects[i].username + '</span></span></p>';
+    }
 }
