aa175ff6ea1e6f7817d9cce7ac6072478dc69e89
Jason Fan <jason.fan.74@gmail.com>
2015-03-08 16:24:08 -0400

    clean up => removed console messages, started README

---
+++ messages/README.md
@@ -0,0 +1,15 @@
+#Jason Fan - COMP 20 Spring 2015 - Lab 6: Messages
+
+###What has been implemented propery:
+* All required aspects have been implemented properly, only ```lab.js``` and ```style.css``` has been changed
+
+###Identify anyone whom you have collaborated and discussed the assigment
+* I discussed the assigment with my most lovely, and ~~fabulous~~ ~~awesome~~ ~~fantastic~~ great roomate Norman Young
+
+###Time spent:
+* approx 45 mins
+
+###Acknowledgements:
+Thanks to Prof. Ming Chow for making his examples for DOM lecture available, I used the stylesheet from his #FML example to format the messages
+
+###Question regarding Same Origin Policy:
--- messages/lab.js
+++ messages/lab.js
@@ -1,17 +1,21 @@
-// Your JavaScript goes here...
+/*  lab.js
+    08/03/15
+    Comp 20 - Lab 6: Messages
+    Jason Fan
+*/
+
 function parse()
 {
         request = new XMLHttpRequest();
         request.open("GET", "data.json",true);
-        request.onreadystatechange = parseData; // prepare function to be called
-        request.send(); //Exececute request
+        request.onreadystatechange = parseData;
+        request.send();
 }
 
 function parseData() 
 {
         if (request.readyState == 4 && request.status == 200) {
             toUpdate = document.getElementById("messages");
-            console.log("got messages!")
             listObjects = JSON.parse(request.responseText);
             for ( i = 0; i < listObjects.length; i++)
                 toUpdate.innerHTML += '<p class="msg">' + listObjects[i].content + 
