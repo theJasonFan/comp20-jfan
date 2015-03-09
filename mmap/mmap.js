8659679ad2204eca1a2e5325514048b9dd47c1a7
Jason Fan <jason.fan.74@gmail.com>
2015-03-08 21:19:24 -0400

    Revert "cleanup"
    This reverts commit eaf5da9bd2c33e5b6a8e53b53ddced726b8d3eb8.
    
    Conflicts:
    	mmap/mmap.js

---
+++ mmap/mmap.js
@@ -0,0 +1,50 @@
+myLat = 0;
+myLng = 0;
+login = "LindyContreras";
+url = "https://secret-about-box.herokuapp.com/sendLocation";
+elem = document.getElementById("info");
+
+function init() 
+{
+    if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+        navigator.geolocation.getCurrentPosition(function(position) {
+            myLat = position.coords.latitude;
+            myLng = position.coords.longitude;
+            postLoc();
+            console.log("3. Leaving the function(position)...");
+        });
+    }
+    else {
+        alert("Geolocation is not supported by your web browser.  What a shame!");
+    }
+}
+
+
+function postLoc()
+{
+    var params = 'login=' + login + '&lat=' + myLat + '&lng=' + myLng;
+    request = new XMLHttpRequest();
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.onreadystatechange = parseResponse;
+    request.send(params);
+    console.log(params);
+    console.log("params sent");
+}
+
+function parseResponse() 
+{
+    console.log("sending params");
+    if (request.readyState == 4 && request.status == 200) {
+        console.log("got data back");
+        toUpdate = document.getElementById("info");
+        data = JSON.parse(request.responseText);
+        for (i = 0; i < data.length; i++)
+            printLocs(data[i]);
+    }
+}
+
+function printLocs(data)
+{
+ elem.innerHTML += "<p>" + data.login + "-" + data.lat + "," + data.lng + "</p>";
+}
